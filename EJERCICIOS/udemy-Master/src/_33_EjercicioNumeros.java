b44913d3d7fcefa0be9f09bd268216d10267e751
Diego Simbaña <d.simbana85@outlook.com>
2022-04-24 12:01:04 +0200

    Correcciones del ejercicio.

--- EJERCICIOS/udemy-Master/src/_33_EjercicioNumeros.java
+++ EJERCICIOS/udemy-Master/src/_33_EjercicioNumeros.java
@@ -4,26 +4,24 @@ public class _33_EjercicioNumeros {
     public static void main(String[] args) {
 
         Scanner scanner;
-        try {
+        Integer n1,n2;
 
+        try {
             scanner = new Scanner(System.in);
             System.out.println("---ORDENAR DE MAYOR A MENOR---");
             System.out.println("Introducir el primer número:");
-            Integer n1 = scanner.nextInt();
+            n1 = scanner.nextInt();
             System.out.println("Introducir el segundo número:");
-            Integer n2 = scanner.nextInt();
+            n2 = scanner.nextInt();
 
-            if (n1 > n2) {
-                System.out.println("Ordenado de mayor a menor");
-                System.out.println(n1 + " - " + n2);
-            } else {
-                System.out.println("Ordenado de mayor a menor");
-                System.out.println(n2 + " - " + n1);
-            }
+            System.out.println("Ordenado de mayor a menor");
+            if (n1 > n2) System.out.println(n1 + " - " + n2);
+            else System.out.println(n2 + " - " + n1);
 
         } catch (Exception e) {
             System.out.println("Error = " + e);
             main(args);
+            System.exit(0);
         }
 
     }
