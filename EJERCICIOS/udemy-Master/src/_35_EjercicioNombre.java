8b4bcd6895f4d94e47209c74f7fc2b19500d563a
Diego Simbaña <d.simbana85@outlook.com>
2022-04-24 21:29:50 +0200

    Se sube el ejercicio de tres nombres familiares.
    Se hace cambios, mejoras.

--- EJERCICIOS/udemy-Master/src/_35_EjercicioNombre.java
+++ EJERCICIOS/udemy-Master/src/_35_EjercicioNombre.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class _35_EjercicioNombre {
     /*
     Obtener el nombre mas largo de tres personas, según los siguientes requerimientos
@@ -11,6 +13,31 @@ public class _35_EjercicioNombre {
     Ejemplo del resultado: "Guillermo Doe tiene el nombre más largo."
      */
     public static void main(String[] args) {
+        String nombre1, nombre2, nombre3;
+        String[] arraynombre1, arraynombre2, arraynombre3;
+
+        try {
+            nombre1 = JOptionPane.showInputDialog("Introduce el nombre 1 y apellido ", "Diego Armando");
+            nombre2 = JOptionPane.showInputDialog(null, "Introducir el nombre 2 y apellido");
+            nombre3 = JOptionPane.showInputDialog(null, "Introducir el nombre 3 y apellido");
+            
+            arraynombre1 = nombre1.split(" ");
+            arraynombre2 = nombre2.split(" ");
+            arraynombre3 = nombre3.split(" ");
+
+            if (arraynombre1[0].length() > arraynombre2[0].length() && arraynombre1[0].length() > arraynombre3[0].length()) {
+                JOptionPane.showMessageDialog(null, nombre1.toUpperCase() + " tiene el nombre más largo.");
+            } else if (arraynombre2[0].length() > arraynombre1[0].length() && arraynombre2[0].length() > arraynombre3[0].length()) {
+                JOptionPane.showMessageDialog(null, nombre2.toUpperCase() + " tiene el nombre más largo.");
+            } else {
+                JOptionPane.showMessageDialog(null, nombre3.toUpperCase() + " tiene el nombre más largo.");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error al introducir los nombres.");
+            main(args);
+            System.exit(0);
+        }
 
     }
 }
