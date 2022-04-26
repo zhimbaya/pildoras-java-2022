3cf9c6dc073fdf5fb5861e2075be9e8f303c199b
Diego Simbaña <d.simbana85@outlook.com>
2022-04-26 19:37:25 +0200

    Subiendo ejercicio de día bisiesto.

---
+++ EJERCICIOS/udemy-Master/src/_37_SentenciaSwitchCaseNombreMes.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class _37_SentenciaSwitchCaseNombreMes {
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        System.out.println("Ingrese el número entre 1 - 12: ");
+        int mes = n.nextInt();
+        String nombreMes = null;
+
+        switch (mes){
+            case 1:
+                nombreMes = "Enero";
+                break;
+            case 2:
+                nombreMes = "Febrero";
+                break;
+            case 3:
+                nombreMes = "Marzo";
+                break;
+            case 4:
+                nombreMes = "Abril";
+                break;
+            case 5:
+                nombreMes = "Mayo";
+                break;
+            case 6:
+                nombreMes = "Junio";
+                break;
+            case 7:
+                nombreMes = "Julio";
+                break;
+            case 8:
+                nombreMes = "Agosto";
+                break;
+            case 9:
+                nombreMes = "Septiembre";
+                break;
+            case 10:
+                nombreMes = "Octubre";
+                break;
+            case 11:
+                nombreMes = "Noviembre";
+                break;
+            case 12:
+                nombreMes = "Diciembre";
+                break;
+            default:
+                System.out.println("Nombre indefinido");
+                break;
+        }
+    }
+}
---
+++ EJERCICIOS/udemy-Master/src/_37_SentenciaSwitchNumDiasMes.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class _37_SentenciaSwitchNumDiasMes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el número del mes de 1 - 12");
+        int mes = scanner.nextInt();
+
+        int numeroDias = 0;
+        System.out.println("Ingrese el año (YYYY)");
+        int anio = scanner.nextInt();
+
+        switch (mes){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                numeroDias = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                numeroDias = 30;
+                break;
+            case 2:
+                if (anio % 400 == 0 || ((anio % 4 == 0) && !(anio % 100 == 0))){
+                    numeroDias = 29;
+                }else{
+                    numeroDias = 28;
+                }
+                break;
+            default:
+                numeroDias = 0;
+
+        }
+        System.out.println(numeroDias);
+    }
+}
