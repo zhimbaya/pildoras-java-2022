ac8efc4f6ce2835404afbb42a7422b6ea7275722
Diego Simbaña <d.simbana85@outlook.com>
2022-07-09 12:23:18 +0200

    Se crea la clase para ordenar.

--- EJERCICIOS/_02_Arrays/src/_02_EjemploArraysBurbujaClase.java
+++ EJERCICIOS/_02_Arrays/src/_02_EjemploArraysBurbujaClase.java
@@ -3,7 +3,7 @@ public class _02_EjemploArraysBurbujaClase {
         int total = arreglo.length;
         for (int i = 0; i < total - 1; i++) {
             for (int j = 0; j < total - 1 - i; j++) {
-                if ( ((Comparable)arreglo[j + 1]).compareTo(arreglo[j]) < 0 ) {
+                if ( ((Comparable)arreglo[j + 1]).compareTo(arreglo[j]) < 0 ) { //CAMBIANDO EL SIGNO LO ORDENAMOS DE FORMA DESCENDENTE
                     Object auxiliar = arreglo[i];
                     arreglo[i] = arreglo[j + 1];
                     arreglo[j + 1] = auxiliar;
@@ -21,6 +21,20 @@ public class _02_EjemploArraysBurbujaClase {
         //Collections.reverse(Arrays.asList(productos));
         for (int i = 0; i < total; i++) {
             System.out.println("productos[" + i + "] = " + productos[i]);
+        }
+
+        Integer[] numeros = new Integer[4];
+        int total2 = numeros.length;
+
+        numeros[0] = 1;
+        numeros[1] = Integer.valueOf("2");
+        numeros[2] = (int) 3L;
+        numeros[3] = 7;
+
+        sortBurbuja(numeros);
+
+        for (int i = 0; i < total2; i++) {
+            System.out.println("números[" + i + "] = " + numeros[i]);
         }
 
     }
